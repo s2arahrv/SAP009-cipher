ee04a18899c376ff5d6353f795a64e6860181c7a
Sarah R. Vieira <sarahrdvieira@gmail.com>
2023-01-19 19:28:48 -0300

    Javascript modificado

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,37 @@
 const cipher = {
   encode : function(offset, string) {
+//Variável da mensagem final
+let result = "";
+//Encontrar código ASC das letras do alfabeto
+//Loop por todas as letras da mensagem, encontrando o código de cada uma
+  for (let i = 0; i < string.length; i++){
+      const cipherUppercase = string.toUpperCase();
+      const cipherCode = cipherUppercase.charCodeAt(i);
+/*Executar ((codigoDaLetra - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra, 
+retornar String dos códigos unicode e adicionar ao output final*/      
+      } return result += string.fromCharCode((cipherCode - 65 + offset) % 26) + 65;
+  } 
+};
+export default cipher;
+ 
+
+
+
+
+
+
+
+
+
+
+
+/*encode : function(offset, string) {
     string = document.getElementById("input").value.toUpperCase();
-    const offset = (letterPosition + 3) % 26;
+    offset = (letterPosition + 3) % 26;
 
     for (let i = 0; i < string.length; i++){
       const letterPosition = string.charCodeAt(i)-65;
       return letterPosition + offset + 65;
     }
     document.getElementById("output").innerHTML = encode(offset, string);
-  }
-};
-// (codigoDaLetra - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra//
-
-export { cipher };
+  },*/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import { encode } from './cipher.js';
+import cipher from './cipher.js';
+//Chamar botão do HTML
+const inputButton = document.getElementById("encodeBt");
+//Dar a ele a função encodeMessage quando for clicado
+inputButton.addEventListener("click", encodeMessage);
+ function encodeMessage()  {
+//Chamar elemento offset, string, conectar cipher.encode a encodeMessage e estabelecer onde aparecerá a mensagem final
+    const offset = Number(document.getElementById("offsetNum").value);
+    const string = document.getElementById("input").value;
+    const encodeResult = cipher.encode(offset, string);
+    const showResult = document.getElementById("outputArea");
 
-console.log(encode);
+    console.log(encodeResult);
+    //return showResult.innerHTML = encodeResult;
+};
+//console.log(cipher.encode);
